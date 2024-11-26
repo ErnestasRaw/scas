68f9f6462051011358887a8f6a78f10f6393c1ef
PC <ignas.rimkus@stud.vilniustech.lt>
2024-11-26 19:27:20 +0200

    .

---
+++ src/components/utils/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "@/app/styles/header.module.css";
+
+
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>SCAS</h1>
+    </header>
+  );
+}
