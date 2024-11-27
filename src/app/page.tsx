376aba24e8c9554e85be80b7bfa32a2cb887349f
PC <ignas.rimkus@stud.vilniustech.lt>
2024-11-27 02:58:02 +0200

    Logo, header update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 import styles from './styles/page.module.css'
-
+import Image from 'next/image'
 export default function HomePage() {
+
+
+
+
+
     return (
       <main className={styles.main}>
+        <div className={styles.Logo}>
+          <Image 
+          
+          src="/images/SCAS.png" 
+          alt="Logo" 
+          width={200} 
+          height={200} 
+          objectFit="contain"/>
+        </div>
       </main>
     );
   }
--- src/components/utils/header.tsx
+++ src/components/utils/header.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import styles from "@/app/styles/header.module.css";
-
-
+import { FiAlignJustify } from "react-icons/fi";
+import { IoMdLogOut } from "react-icons/io";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <header className={styles.header}>
-      <h1>SCAS</h1>
+      <div className={styles.Hamburger} onClick={toggleMenu}>
+        <FiAlignJustify size={24} />
+      </div>
+
+      <nav className={`${styles.Nav} ${isOpen ? styles.Show : ""}`}>
+        <ul>
+          <li>Prisijungęs, kaip ...Value...</li>
+        </ul>
+        <div className={styles.Logout} title="Atsijungti">
+          <IoMdLogOut />
+        </div>
+      </nav>
     </header>
   );
 }
